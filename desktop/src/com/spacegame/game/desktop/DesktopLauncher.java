87af76ac3c8b0762d2f98e29adec945726ed68e9
Will Greenberg <will@endlessm.com>
2015-10-17 10:56:03 -0700

    Bootstrapped from the gdx-setup.jar

---
+++ desktop/src/com/spacegame/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spacegame.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spacegame.game.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}
---
+++ html/src/com/spacegame/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.spacegame.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.spacegame.game.SpaceGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SpaceGame();
+        }
+}

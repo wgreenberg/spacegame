edee291f5622c3641279fdd1bcba6e14d78c9124
Will Greenberg <will@endlessm.com>
2015-10-17 11:34:45 -0700

    tabs by default? wtf is this 1999

--- core/src/com/spacegame/game/SpaceGame.java
+++ core/src/com/spacegame/game/SpaceGame.java
@@ -10,21 +10,21 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SpaceGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture texture;
+    SpriteBatch batch;
+    Texture texture;
     Pixmap pixmap;
     Sprite sprite;
 
     int cDim = 20;
     int offset = 0;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-	}
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+    }
 
-	@Override
-	public void render () {
+    @Override
+    public void render () {
         // I'm 99% we shouldn't ever actually do this kinda shit in the render cycle
         pixmap = new Pixmap(256, 256, Pixmap.Format.RGBA8888);
         int white = 0xFFFFFFFF;
@@ -55,10 +55,10 @@ public class SpaceGame extends ApplicationAdapter {
         pixmap.dispose();
         sprite = new Sprite(texture);
 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-	}
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+    }
 }
